2cd1da113356f559522322cc5cededd68ab202ef
FriendlyStepBro <logicye@proton.me>
2024-10-20 23:36:56 +1000

    Added number to acvtive line in vim init script

--- nvim/lua/vim-config.lua
+++ nvim/lua/vim-config.lua
@@ -3,6 +3,7 @@ vim.cmd("set tabstop=3")
 vim.cmd("set softtabstop=3")
 vim.cmd("set shiftwidth=3")
 vim.cmd("set relativenumber")
+vim.cmd("set number")
 vim.cmd("set scrolloff=999")
 vim.g.mapleader = " "
 vim.cmd("set mouse=")
